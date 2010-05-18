b3a7ff656137e5b0ca2f1d4f3c110496ec9d7986
Daniel X. Moore <yahivin@gmail.com>
2010-05-18 18:48:55 +0000

    Cleaning up the main game loop

--- javascripts/contrasaurus/UI.js
+++ javascripts/contrasaurus/UI.js
@@ -123,3 +123,17 @@ function Scene(backgrounds, foregrounds) {
     drawForegrounds: drawLayersGenerator(foregrounds)
   }
 }
+
+function healthColorMap(completeness) {
+  var r = Math.floor(Math.clamp(1.5 - 2 * completeness, 0, 1) * 255).toString(16);
+  if(r.length == 1) {
+    r = "0" + r;
+  }
+
+  var g = Math.floor(Math.clamp(completeness * 0.75, 0, 1) * 255).toString(16);
+  if(g.length == 1) {
+    g = "0" + g;
+  }
+
+  return "#" + r + g + "00";
+}
