b28678eb913edeea1aa6fd58cd76591518549043
Daniel X. Moore <yahivin@gmail.com>
2010-05-20 06:51:26 +0000

    Added bomb image and bomb rotation

--- javascripts/contrasaurus/bomb.js
+++ javascripts/contrasaurus/bomb.js
@@ -1,13 +1,20 @@
 function Bomb(launchAngle, I) {
   I = I || {};
 
+  var bombTile = loadImageTile("images/bomb.png");
+
   $.reverseMerge(I, {
     color: "#000",
-    width: 10,
-    collideDamage: 1
+    width: 64,
+    height: 23,
+    collideDamage: 5
   });
 
   var self = Bullet(launchAngle, I).extend({
+    draw: function(canvas) {
+      bombTile.draw(canvas, I.x, I.y, {rotation: -Math.atan2(I.yVelocity, I.xVelocity)});
+    },
+
     after: {
       hit: function() {
         I.active = false;
