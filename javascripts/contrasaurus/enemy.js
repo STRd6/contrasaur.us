d3e9ee0c1a9ec7d040ac4dda699c826ac00cc193
Matt Diebolt <mdiebolt@gmail.com>
2010-05-27 06:12:58 +0000

    parasoldiers refactored into own class. Probably still need to fix up the image so the landing is smoother

--- javascripts/contrasaurus/enemy.js
+++ javascripts/contrasaurus/enemy.js
@@ -2,31 +2,16 @@ function Enemy(I) {
   I = I || {};
 
   var soldierTile = loadImageTile("images/soldier.png");
-  var parachuteActiveTile = loadImageTile("images/parasoldier.png");
-  var airborne;
-  var startingHeight;
-  var groundSoldier;
 
-  var startingY;
-  if (Math.random() < 0.5) {
-    startingY = 0;
-    airborne = true;
-    // HACK
-    startingHeight = 90;
-  } else {
-    startingY = 340;
-    airborne = false;
-    groundSoldier = true;
-  }
-
+  // TODO: make them not shoot into the ground
   var theta = Math.random() * (Math.PI * 2);
 
   $.reverseMerge(I, {
     x: rand(canvas.width()),
-    y: startingY,
+    y: 340,
     width: 10,
-    height: startingHeight || 53,
-    yVelocity: 3,
+    height: 40,
+    yVelocity: 0,
     health: 3,
     color: "#F00",
     collideDamage: 1,
@@ -46,17 +31,7 @@ function Enemy(I) {
   });
 
   var self = GameObject(I).extend({
-    land: function(h) {
-      I.y = h - I.height;
-      I.yVelocity = 0;
-      airborne = false;
-      if (!groundSoldier) {
-        I.sprite = soldierTile;
-        // HACK to compensate for parachute image being taller
-        // than soldier image
-        I.y += 40;
-      }
-    },
+    land: function(h) { },
     after: {
       hit: function(other) {
         if(other.bump) {
@@ -65,13 +40,6 @@ function Enemy(I) {
       },
       update: function() {
         I.shootLogic();
-      },
-      draw: function() {
-        if (airborne) {
-          I.sprite = parachuteActiveTile;
-        } else {
-          I.sprite = soldierTile;
-        }
       }
     }
   });
