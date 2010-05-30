43bb2df784e5a7ec56ebbf9e738d252b637db7f1
Matt Diebolt <mdiebolt@gmail.com>
2010-05-30 20:45:36 +0000

    added site javascripts

---
+++ site/javascripts/util.js
@@ -0,0 +1,104 @@
+function rand(n) {
+  return Math.floor(Math.random() * n);
+}
+
+function distance(p1, p2) {
+  return Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
+}
+
+/**
+ * Randomly select an element from the array.
+ *
+ * @return a random element from an array
+ */
+Array.prototype.rand = function() {
+  return this[rand(this.length)];
+};
+
+/**
+ * Returns a mod useful for array wrapping.
+ *
+ * @param {Number} n
+ * @param {Number} base
+ */
+Math.mod = function(n, base) {
+  var result = n % base;
+
+  if(result < 0 && base > 0) {
+    result += base;
+  }
+
+  return result;
+};
+
+Math.clamp = function(val, min, max) {
+  return Math.min(Math.max(val, min), max);
+};
+
+Number.prototype.times = function(iterator, context) {
+  for(var i = 0; i < this; i++) {
+    iterator.call(context, i);
+  }
+  return i;
+}
+
+function before(method, interception) {
+  return function() {
+    interception.apply(this, arguments);
+    return method.apply(this, arguments);
+  };
+}
+
+function after(method, interception) {
+  return function() {
+    var returnValue = method.apply(this, arguments);
+    interception.apply(this, arguments);
+    return returnValue;
+  };
+}
+
+/**
+ * Merges properties from objects into target without overiding.
+ * First come, first served.
+ * @return target
+ */
+jQuery.extend({
+  reverseMerge: function(target) {
+    var i = 1, length = arguments.length;
+    
+    for( ; i < length; i++) {
+      var object = arguments[i];
+
+      for(var name in object) {
+        if(!target.hasOwnProperty(name)) {
+          target[name] = object[name];
+        }
+      }
+    }
+    
+    return target;
+  }
+});
+
+function collision(A, B) {
+  var b = A.boundingBox();
+  var t = B.boundingBox();
+
+  var xOverlap = (b.x < t.x && b.x + b.width >= t.x) ||
+    (t.x < b.x && t.x + t.width >= b.x);
+  var yOverlap = (b.y < t.y && b.y + b.height >= t.y) ||
+    (t.y < b.y && t.y + t.height >= b.y);
+  if (A.active() && B.active()) {
+    if(xOverlap && yOverlap) {
+      A.hit(B);
+      B.hit(A);
+    }
+  }
+}
+
+function transformPoint(point, transformMatrix) {
+  return {
+    x: transformMatrix.a * point.x + transformMatrix.b * point.y,
+    y: transformMatrix.c * point.x + transformMatrix.d * point.y
+  }
+}
