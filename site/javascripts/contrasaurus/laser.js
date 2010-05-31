3b2fa2e73b487597b8e5476ab1343ebac573d76c
Daniel X. Moore <yahivin@gmail.com>
2010-05-31 20:49:51 +0000

    Added hitbox to laser

--- site/javascripts/contrasaurus/laser.js
+++ site/javascripts/contrasaurus/laser.js
@@ -6,15 +6,16 @@ function Laser(theta, I) {
     width: 32,
     height: 4,
     collideDamage: 3,
+    radius: 2,
     sprite: loadImageTile("images/laser.png"),
     xVelocity: Math.cos(theta)*speed,
     yVelocity: Math.sin(theta)*speed
   });
 
   var self = Bullet(theta, I).extend({
-    hit: function(other) { },
-    draw: function(canvas) {
-      I.sprite.draw(canvas, I.x, I.y, {rotation: Math.atan2(I.yVelocity, I.xVelocity)});
+    hit: $.noop,
+    getTransform: function() {
+      return rotationTransform(Math.atan2(I.yVelocity, I.xVelocity));
     }
   });
   return self;
