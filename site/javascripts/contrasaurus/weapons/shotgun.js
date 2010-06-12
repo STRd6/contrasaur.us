5eb566ddd59213f289dfa99804096660eabdd013
Daniel X. Moore <yahivin@gmail.com>
2010-06-12 22:52:37 +0000

    Fixed shotgun position call

--- site/javascripts/contrasaurus/weapons/shotgun.js
+++ site/javascripts/contrasaurus/weapons/shotgun.js
@@ -13,23 +13,23 @@ function Shotgun(I) {
 
   var self = Weapon(I).extend({
 
-    shoot: function(midpoint, transform) {
+    shoot: function(position, transform) {
 
       if(rand(100) < I.power) {
-        var target = currentLevel.nearestEnemy(I.dino.position());
+        var target = currentLevel.nearestEnemy(position);
         var direction;
 
         if(target) {
           var targetMidpoint = target.midpoint();
-          var targetDistance = distance(midpoint, targetMidpoint);
+          var targetDistance = distance(position, targetMidpoint);
           var targetVelocity = target.velocity();
 
           targetMidpoint.y += (targetDistance / 10) * targetVelocity.y;
           targetMidpoint.x += (targetDistance / 10) * targetVelocity.x;
 
           direction = Math.atan2(
-            targetMidpoint.y - midpoint.y,
-            targetMidpoint.x - midpoint.x
+            targetMidpoint.y - position.y,
+            targetMidpoint.x - position.x
           );
         } else {
           direction = target ? Math.atan2(I.yVelocity, I.xVelocity) : 0;
@@ -40,8 +40,8 @@ function Shotgun(I) {
             return Math.random() * 20 - 10;
           }
 
-          var x = midpoint.x + fuzz();
-          var y = midpoint.y + fuzz() * 2;
+          var x = position.x + fuzz();
+          var y = position.y + fuzz() * 2;
 
           addGameObject(Bullet(direction, { x: x, y: y }));
         });
