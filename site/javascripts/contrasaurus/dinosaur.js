2434f2de4d1449eed7615ce50d6ec2a89004854c
Matt Diebolt <mdiebolt@gmail.com>
2010-06-13 09:09:54 +0000

    removed shield from dino

--- site/javascripts/contrasaurus/dinosaur.js
+++ site/javascripts/contrasaurus/dinosaur.js
@@ -7,7 +7,7 @@ function Dinosaur() {
   var machineGun = MachineGun();
   var chainsaw = Chainsaw();
   var jetpack = Jetpack();
-  var shield = Shield();
+  //var shield = Shield();
 
   var x = (CANVAS_WIDTH - width) / 2;
   var y = 150;
@@ -143,7 +143,7 @@ function Dinosaur() {
 
         chainsaw.draw(canvas);
         laserGun.draw(canvas);
-        shield.draw(canvas);
+        //shield.draw(canvas);
       });
 
       // TO DO call draw on each weapon
