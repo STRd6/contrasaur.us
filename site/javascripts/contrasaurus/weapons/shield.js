7ffccb20edafed6b900b91fce48ea587a5965d7c
Matt Diebolt <mdiebolt@gmail.com>
2010-06-25 04:36:33 +0000

    shield doesn"t expire

--- site/javascripts/contrasaurus/weapons/shield.js
+++ site/javascripts/contrasaurus/weapons/shield.js
@@ -4,11 +4,15 @@ function Shield(I) {
   $.reverseMerge(I, {
     age: 0,
     collideDamage: 0,
+    duration: -1,
     effectCount: 3,
+    health: 50,
     radius: 72,
     sprite: Sprite.load("images/weapons/shield.png")
   });
 
-  var self = Weapon(I).extend({ });
+  var self = Weapon(I).extend({
+    shoot: $.noop
+  });
   return self;
 }
