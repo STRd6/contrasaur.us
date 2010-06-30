9843e22e87d32451dd32aa79e92b36572447a769
Daniel X. Moore <yahivin@gmail.com>
2010-06-30 03:46:29 +0000

    Added multi-frame hit circles

--- site/javascripts/contrasaurus/dinosaur.js
+++ site/javascripts/contrasaurus/dinosaur.js
@@ -16,7 +16,17 @@ function Dinosaur() {
 
   var pitchAngle = 0;
 
-  var walkTile = loadAnimation("images/contrasaurus/walk.png", 8, 283, 163, 3);
+  var walkAnimation = loadAnimation("images/contrasaurus/walk.png", 8, 283, 163, 3);
+  var walkFrameCircles = [
+    [{"x":87,"y":-52,"radius":27},{"x":13,"y":-10,"radius":37},{"x":117,"y":-45,"radius":24},{"x":50,"y":-38,"radius":19},{"x":-27,"y":-12,"radius":25},{"x":-60,"y":-2,"radius":18},{"x":-89,"y":5,"radius":14},{"x":-113,"y":10,"radius":12},{"x":-2,"y":35,"radius":20},{"x":0,"y":64,"radius":18},{"x":24,"y":72,"radius":10},{"x":39,"y":73,"radius":8},{"x":-130,"y":11,"radius":8}],
+    [{"x":87,"y":-52,"radius":27},{"x":12,"y":-8,"radius":41},{"x":117,"y":-45,"radius":24},{"x":50,"y":-38,"radius":19},{"x":-22,"y":-13,"radius":26},{"x":-60,"y":-2,"radius":18},{"x":-89,"y":5,"radius":14},{"x":-112,"y":10,"radius":12},{"x":-130,"y":10,"radius":8},{"x":26,"y":39,"radius":9},{"x":-19,"y":43,"radius":10},{"x":69,"y":63,"radius":14},{"x":-16,"y":65,"radius":18},{"x":-9,"y":28,"radius":10},{"x":48,"y":56,"radius":9},{"x":9,"y":73,"radius":10},{"x":35,"y":50,"radius":8}],
+    [{"x":87,"y":-53,"radius":27},{"x":12,"y":-9,"radius":38},{"x":118,"y":-45,"radius":23},{"x":50,"y":-38,"radius":19},{"x":-21,"y":-12,"radius":28},{"x":-60,"y":-6,"radius":18},{"x":-88,"y":0,"radius":14},{"x":-113,"y":1,"radius":12},{"x":-127,"y":0,"radius":8},{"x":-37,"y":59,"radius":25},{"x":51,"y":70,"radius":12},{"x":-34,"y":31,"radius":11},{"x":35,"y":19,"radius":12},{"x":42,"y":53,"radius":10},{"x":37,"y":37,"radius":8},{"x":89,"y":60,"radius":10},{"x":67,"y":71,"radius":7},{"x":79,"y":67,"radius":7},{"x":-18,"y":24,"radius":8}],
+    [{"x":85,"y":-54,"radius":27},{"x":9,"y":-6,"radius":42},{"x":117,"y":-45,"radius":24},{"x":49,"y":-36,"radius":18},{"x":-26,"y":-4,"radius":31},{"x":-60,"y":-5,"radius":17},{"x":-88,"y":2,"radius":15},{"x":-112,"y":4,"radius":12},{"x":-130,"y":2,"radius":7},{"x":28,"y":74,"radius":9},{"x":-84,"y":63,"radius":18},{"x":13,"y":44,"radius":10},{"x":18,"y":61,"radius":8},{"x":67,"y":74,"radius":9},{"x":43,"y":76,"radius":7},{"x":54,"y":76,"radius":6},{"x":-66,"y":48,"radius":8},{"x":-47,"y":27,"radius":8},{"x":-60,"y":34,"radius":10}],
+    [{"x":86,"y":-53,"radius":27},{"x":13,"y":-10,"radius":37},{"x":117,"y":-45,"radius":24},{"x":50,"y":-38,"radius":19},{"x":-27,"y":-12,"radius":25},{"x":-60,"y":-2,"radius":18},{"x":-87,"y":6,"radius":15},{"x":-112,"y":10,"radius":13},{"x":-130,"y":12,"radius":8},{"x":-2,"y":64,"radius":20},{"x":-7,"y":34,"radius":20},{"x":37,"y":73,"radius":11},{"x":20,"y":74,"radius":9}],
+    [{"x":87,"y":-52,"radius":27},{"x":13,"y":-11,"radius":38},{"x":118,"y":-43,"radius":24},{"x":49,"y":-39,"radius":19},{"x":-25,"y":-14,"radius":24},{"x":-60,"y":-3,"radius":17},{"x":-89,"y":5,"radius":14},{"x":-112,"y":8,"radius":11},{"x":-129,"y":9,"radius":7},{"x":-11,"y":58,"radius":25},{"x":4,"y":40,"radius":27},{"x":80,"y":65,"radius":12},{"x":66,"y":54,"radius":11},{"x":37,"y":52,"radius":10},{"x":49,"y":54,"radius":10}],
+    [{"x":86,"y":-53,"radius":27},{"x":12,"y":-8,"radius":41},{"x":117,"y":-45,"radius":24},{"x":50,"y":-38,"radius":19},{"x":-24,"y":-13,"radius":25},{"x":-60,"y":-6,"radius":18},{"x":-86,"y":-2,"radius":15},{"x":-109,"y":0,"radius":12},{"x":-127,"y":-1,"radius":8},{"x":-40,"y":59,"radius":25},{"x":56,"y":72,"radius":12},{"x":-33,"y":31,"radius":11},{"x":-18,"y":25,"radius":7},{"x":37,"y":27,"radius":10},{"x":36,"y":44,"radius":10},{"x":43,"y":59,"radius":8},{"x":74,"y":67,"radius":9},{"x":90,"y":57,"radius":11}],
+    [{"x":85,"y":-54,"radius":27},{"x":5,"y":-5,"radius":44},{"x":117,"y":-45,"radius":24},{"x":50,"y":-37,"radius":19},{"x":-33,"y":1,"radius":33},{"x":-62,"y":-5,"radius":18},{"x":-90,"y":1,"radius":14},{"x":-113,"y":2,"radius":11},{"x":-128,"y":1,"radius":7},{"x":-89,"y":66,"radius":13},{"x":68,"y":73,"radius":10},{"x":48,"y":74,"radius":10},{"x":29,"y":72,"radius":10},{"x":-62,"y":38,"radius":15},{"x":16,"y":59,"radius":10},{"x":-71,"y":58,"radius":9},{"x":12,"y":44,"radius":8}]
+  ];
   var parasailTile = Sprite.load("images/levels/parasail/parasail.png");
   
   var I = {
@@ -31,8 +41,8 @@ function Dinosaur() {
     yVelocity: 6,
     collideDamage: 2,
     collisionType: "dino",
-    hitCircles: [{"x":87,"y":-52,"radius":27},{"x":13,"y":-10,"radius":37},{"x":117,"y":-45,"radius":24},{"x":50,"y":-38,"radius":19},{"x":-27,"y":-12,"radius":25},{"x":-60,"y":-2,"radius":18},{"x":-89,"y":5,"radius":14},{"x":-114,"y":10,"radius":12}],
-    sprite: walkTile
+    hitCircles: walkFrameCircles[0],
+    sprite: walkAnimation
   };
 
   var accessories = [];
@@ -102,9 +112,9 @@ function Dinosaur() {
           parasailTile.draw(canvas, -100, -100);
         }
 
-        walkTile.draw(canvas,
-          -walkTile.width/2,
-          -walkTile.height/2
+        walkAnimation.draw(canvas,
+          -walkAnimation.width/2,
+          -walkAnimation.height/2
         );
 
         $.each(accessories, function(i, accessory) {
@@ -151,6 +161,8 @@ function Dinosaur() {
 
     after: {
       update: function(position) {
+        I.hitCircles = walkFrameCircles[walkAnimation.frame()];
+
         jetpack.update();
 
         if(parasailing) {
