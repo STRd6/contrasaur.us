805bc1673a3f850c0a13e5efc95135e8734a55e1
Daniel X. Moore <yahivin@gmail.com>
2010-07-21 05:33:17 +0000

    Jump, jump, jump

---
+++ site/javascripts/contrasaurus/enemies/boat.js
@@ -0,0 +1,51 @@
+function Boat(I) {
+  I = I || {};
+
+  $.reverseMerge(I, {
+    collisionType: "dino",
+    health: 10000,
+    sprite: Sprite.load("images/levels/parasail/speed-boat.png")
+  });
+
+  var jumping = false;
+  var jumpImpulse = -10;
+
+  var self = GameObject(I).extend({
+    getTransform: function() {
+      var t;
+      if(jumping) {
+        t =  Matrix.rotation((Math.PI / 6) * I.yVelocity / -jumpImpulse);
+      } else {
+        t = Matrix.IDENTITY;
+      }
+      return t.translate(I.x, I.y);
+    },
+
+    before: {
+      update: function(position) {
+        // Oscillate
+        I.x = position.x + boatTarget.x + 20 * Math.sin(I.age/20);
+
+        if(jumping) {
+          I.yVelocity += GRAVITY / 2;
+
+          if(I.y >= boatTarget.y) {
+            I.y = boatTarget.y;
+            I.yVelocity = 0;
+            jumping = false;
+          }
+        } else {
+          if(rand(100) == 0) {
+            jumping = true;
+
+            I.yVelocity = jumpImpulse;
+          }
+        }
+      }
+    }
+  });
+
+  var boatTarget = Point(I.x, I.y);
+
+  return self;
+}
--- site/javascripts/contrasaurus/levels/boat.js
+++ site/javascripts/contrasaurus/levels/boat.js
@@ -1,7 +1,4 @@
 (function() {
-  var boatTarget = Point(440, 285);
-  var boatPosition =  boatTarget.add(Point());
-
   var scene = Scene([
     {
       image: Sprite.load("images/levels/parasail/coast.png"),
@@ -35,11 +32,6 @@
       width: 640
     },
     {
-      image: Sprite.load("images/levels/parasail/speed-boat.png"),
-      parallaxRate: 0,
-      position: boatPosition
-    },
-    {
       image: Sprite.load("images/levels/parasail/ocean.png"),
       parallaxRate: 1,
       velocity: Point(-3, 0),
@@ -64,17 +56,19 @@
     event: function(level) {
       dino.parasailing(true);
 
+      var boat = Boat({
+        x: 440,
+        y: 305
+      });
+
+      level.addGameObject(boat);
+
       level.tiltAmount(5);
     }
   }, {
     every: 1,
     event: function(level) {
-      counter++;
-
-      // Oscilate Boat
-      boatPosition.x = boatTarget.x + 20 * Math.sin(counter/20);
-
-      // Update speed and position based on boat and cord elasticity
+      // Update dino speed and position based on boat and cord elasticity
 
     }
   }, {
