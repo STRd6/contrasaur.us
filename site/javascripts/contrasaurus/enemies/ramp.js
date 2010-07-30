8c184c076b8941ed226bfeabebfab83fc350a621
Daniel X. Moore <yahivin@gmail.com>
2010-07-30 17:27:37 +0000

    Added ramp js and image

---
+++ site/javascripts/contrasaurus/enemies/ramp.js
@@ -0,0 +1,31 @@
+function Ramp(I) {
+  I = I || {};
+
+  $.reverseMerge(I, {
+    collisionType: "enemy",
+    health: 10,
+    sprite: Sprite.load("images/enemies/ramp.png")
+  });
+
+  var checkBounds = GameObject.generateCheckBounds(I, 100);
+
+  var self = GameObject(I).extend({
+    bulletHitEffect: Enemy.sparkSprayEffect,
+
+    sink: $.noop,
+
+    after: {
+      hit: function(other) {
+        if(other.bounce) {
+          other.bounce();
+        }
+      },
+
+      update: function() {
+        checkBounds.apply(self, arguments);
+      }
+    }
+  });
+
+  return self;
+}
