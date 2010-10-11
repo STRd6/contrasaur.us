0225694d355514b6fe406f33eb41529f785d1ff0
Matt Diebolt <mdiebolt@gmail.com>
2010-10-11 01:31:28 +0000

    removed bullet hitting flesh and bullet hitting dirt sound effects. They were causing the bite not to play because the max number of sound channels was being reached

--- src/javascripts/contrasaurus/enemies/enemy.js
+++ src/javascripts/contrasaurus/enemies/enemy.js
@@ -111,7 +111,7 @@ function Enemy(I) {
 }
 
 Enemy.bloodSprayEffect = function(bullet) {
-  Sound.play("bullet_hit_flesh", 1);
+  //Sound.play("bullet_hit_flesh", 1);
 
   bullet.effectCount().times(function() {
     var point = bullet.position();
