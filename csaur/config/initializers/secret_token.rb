931b72f2f8bcaa0fbd54804d9fe11f9d2d5addf2
Daniel X Moore <yahivin@gmail.com>
2010-11-22 14:08:44 -0800

    Added rails app for auth dealies

---
+++ csaur/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Csaur::Application.config.secret_token = '70026b7b7c1f915b5f4e855c3c3dccd79378e657335598c0721305b5df7fad5240262ef29f4104a27b12faae8bad76dd255da035a98f78b2947134ef0d71c5ae'
