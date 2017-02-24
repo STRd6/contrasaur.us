3b406da98d5e5b1e66c327cc86a7a7fc4968d9da
Daniel X. Moore <yahivin@gmail.com>
2017-02-24 15:09:10 -0800

    Added basic event tracking

--- javascripts/contrasaurus.js
+++ javascripts/contrasaurus.js
@@ -2652,6 +2652,149 @@ function healthColorMap(completeness) {
 
   return "#" + r + g + "00";
 }
+/*
+
+Postmaster wraps the `postMessage` API with promises.
+ */
+var Postmaster = (function () {
+var Postmaster, ackTimeout, defaultReceiver,
+  slice = [].slice;
+
+defaultReceiver = self;
+
+ackTimeout = 1000;
+
+Postmaster = function(self) {
+  var dominant, pendingResponses, remoteId, send;
+  if (self == null) {
+    self = {};
+  }
+  send = function(data) {
+    var target;
+    target = self.remoteTarget();
+    if ((typeof Worker === "undefined" || Worker === null) || target instanceof Worker) {
+      return target.postMessage(data);
+    } else {
+      return target.postMessage(data, "*");
+    }
+  };
+  dominant = Postmaster.dominant();
+  if (self.remoteTarget == null) {
+    self.remoteTarget = function() {
+      return dominant;
+    };
+  }
+  if (self.receiver == null) {
+    self.receiver = function() {
+      return defaultReceiver;
+    };
+  }
+  if (self.ackTimeout == null) {
+    self.ackTimeout = function() {
+      return ackTimeout;
+    };
+  }
+  self.receiver().addEventListener("message", function(event) {
+    var data, id, method, params, ref, type;
+    if (event.source === self.remoteTarget() || !event.source) {
+      data = event.data;
+      type = data.type, method = data.method, params = data.params, id = data.id;
+      switch (type) {
+        case "ack":
+          return (ref = pendingResponses[id]) != null ? ref.ack = true : void 0;
+        case "response":
+          return pendingResponses[id].resolve(data.result);
+        case "error":
+          return pendingResponses[id].reject(data.error);
+        case "message":
+          send({
+            type: "ack",
+            id: id
+          });
+          return Promise.resolve().then(function() {
+            if (typeof self[method] === "function") {
+              return self[method].apply(self, params);
+            } else {
+              throw new Error("`" + method + "` is not a function");
+            }
+          }).then(function(result) {
+            return send({
+              type: "response",
+              id: id,
+              result: result
+            });
+          })["catch"](function(error) {
+            var message;
+            if (typeof error === "string") {
+              message = error;
+            } else {
+              message = error.message;
+            }
+            return send({
+              type: "error",
+              id: id,
+              error: {
+                message: message,
+                stack: error.stack
+              }
+            });
+          });
+      }
+    }
+  });
+  pendingResponses = {};
+  remoteId = 0;
+  self.invokeRemote = function() {
+    var id, method, params;
+    method = arguments[0], params = 2 <= arguments.length ? slice.call(arguments, 1) : [];
+    id = remoteId++;
+    send({
+      type: "message",
+      method: method,
+      params: params,
+      id: id
+    });
+    return new Promise(function(resolve, reject) {
+      var ackWait, clear, timeout;
+      clear = function() {
+        clearTimeout(pendingResponses[id].timeout);
+        return delete pendingResponses[id];
+      };
+      ackWait = self.ackTimeout();
+      timeout = setTimeout(function() {
+        var pendingResponse;
+        pendingResponse = pendingResponses[id];
+        if (pendingResponse && !pendingResponse.ack) {
+          clear();
+          return reject(new Error("No ack received within " + ackWait));
+        }
+      }, ackWait);
+      return pendingResponses[id] = {
+        timeout: timeout,
+        resolve: function(result) {
+          clear();
+          return resolve(result);
+        },
+        reject: function(error) {
+          clear();
+          return reject(error);
+        }
+      };
+    });
+  };
+  return self;
+};
+
+Postmaster.dominant = function() {
+  if (typeof window !== "undefined" && window !== null) {
+    return opener || ((parent !== window) && parent) || void 0;
+  } else {
+    return self;
+  }
+};
+
+return Postmaster;
+}());
 function Bindable() {
 
   var eventCallbacks = {};
@@ -3274,6 +3417,7 @@ function Dinosaur(I) {
         });
 
         self.addWeapon(jetpack);
+        trackEvent("jetpack");
       }
     },
 
@@ -3290,6 +3434,8 @@ function Dinosaur(I) {
         if(weapon.selectable()) {
           selectedWeapon = weapon;
         }
+
+        trackEvent("weapon", weapon);
       }
 
       Sound.play("reload");
@@ -3898,6 +4044,18 @@ var canvas;
 var dino;
 var healthBar;
 
+var postmaster = Postmaster();
+var _slice = [].slice
+
+function trackEvent(/* args... */) {
+  console.log("trackEvent", arguments);
+  if(postmaster.remoteTarget()) {
+    var args = _slice.call(arguments, 0);
+    args.unshift("event")
+    postmaster.invokeRemote.apply(postmaster, args);
+  }
+}
+
 Array.prototype.unique = function () {
   var r = new Array();
   o:for(var i = 0, n = this.length; i < n; i++)
@@ -4017,6 +4175,7 @@ function nextStage(choice) {
   } else {
     if(!gameOver) {
       currentStage++;
+      trackEvent("stage", currentStage);
 
       saveGame();
 
@@ -4025,6 +4184,7 @@ function nextStage(choice) {
       } else if(currentStage >= stages.length) {
         endGame();
         alert("You Win!");
+        trackEvent("win");
       } else {
         currentLevel = stages[currentStage];
         stages[currentStage].start(canvas);
